2a59a1c73bc48764bdc6f1613042cae0837b8fa2
KKimRc <shirley_inf0609@163.com>
2024-11-19 13:50:50 +0800

    ADD DEEPSEEK MODEL FOR ALL_AVAILABLE_MODELS

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
@@ -65,8 +65,6 @@ GPT4_MODELS: Dict[str, int] = {
     # 0314 models
     "gpt-4-0314": 8192,
     "gpt-4-32k-0314": 32768,
-    # deepseek models
-    "deepseek-chat": 128000,
 }
 
 AZURE_TURBO_MODELS: Dict[str, int] = {
@@ -126,6 +124,8 @@ ALL_AVAILABLE_MODELS = {
     **GPT3_5_MODELS,
     **GPT3_MODELS,
     **AZURE_TURBO_MODELS,
+    # deepseek models
+    "deepseek-chat": 128000, #添加deepseek模型
 }
 
 CHAT_MODELS = {
